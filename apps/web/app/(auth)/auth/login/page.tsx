0f8bfc0fce157d647ccc5f64a000c92116864619
bossadizenith <org.codeen@gmail.com>
2026-03-03 00:17:00 +0100

    update page.tsx

--- apps/web/app/(auth)/auth/login/page.tsx
+++ apps/web/app/(auth)/auth/login/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center relative bg-black min-h-screen text-white justify-center p-20">
+    <div className="flex flex-col items-center relative bg-background min-h-screen text-foreground justify-center p-20">
       <SocialButton />
     </div>
   );
